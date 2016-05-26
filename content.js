0af15d1a7a5bf48b44a59f7ad052f3b635a9e9cf
mannykoum <manykoum@msn.com>
2016-05-26 14:19:21 -0400

    Finished the extension
    Added a fixed position div to show the sum and average the range of prices when that happens.

--- content.js
+++ content.js
@@ -1,12 +1,9 @@
-//var elements = document.getElementsByTagName('*');
-console.log("LOLOLO");
 // Function to findById with regex (recursively)
 function find_by_id(element, re, tag_name, s) {
 
-	tag_name = tag_name || 'DIV';
+	tag_name = tag_name || 'DIV'; // div is the default tag
     s = s || [];
     if (element.tagName === tag_name && re.exec(element.id) !== null) {
-    //if (re.exec(element.id) !== null) {
         s.push(element);
     }
 
@@ -18,15 +15,19 @@ function find_by_id(element, re, tag_name, s) {
    	return s;
 }
 
-// Find prices and convert them to numbers
-var number, numbers = [];
+// Find prices and convert them to numbers (takes care of range by averaging)
+var number, numbers = [], text, total = 0, ave;
 price_spans = find_by_id(document.body, /^itemPrice_/, 'SPAN');
 $.each(price_spans,function() {
-    console.log(this.textContent);
     // NOTE: it doesn't handle ',' for Europe for example (locale issue)
-	number = Number(this.textContent.replace(/[^0-9\.]+/g,""));
-	numbers.push(number);
-	console.log(number);
+	text = this.textContent.split('-');
+	$.each(text, function(){
+		number = Number(this.replace(/[^0-9\.]+/g,""));
+		total += number;
+	});
+	ave = total/text.length;
+	numbers.push(ave);
+	total = 0;
 });
 
 
@@ -36,4 +37,22 @@ $.each(numbers,function() {
     sum += this;
 });
 
+var div = document.createElement( 'div' );
+var p = document.createElement('p');
+
+// Append elements
+document.body.appendChild(div);
+div.appendChild(p);
+
+div.style.position = 'fixed';
+div.style.bottom = '10%';
+div.style.left = '10%';
+div.style.border = '3px outset rgba(35, 47, 62, 0.7)';
+div.style.borderRadius = '10px';
+div.style.padding = '5px';
+div.style.backgroundColor = '#f5f5f5';
+
+p.style.color = '#444444';
+p.innerHTML = 'Total Cost: '+sum.toFixed(2);
 console.log(sum);
+
